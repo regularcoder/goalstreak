88c84e9b2bc34b1fee75bdf74d012ea059607aa4
Nikhil R <osergularcoder@gmail.com>
2025-01-09 19:36:32 +0530

    Nikhil: Basic calendar view with checkboxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import {useState} from "react";
+
+/*
+TODO:
+- Allow specifying start date (set minDate to this)
+- Calculate streak
+
+- Fetch checked state from a function
+ */
 
 function App() {
-  const [count, setCount] = useState(0)
+    const maxDate = new Date();
+    const [minDate, setMinDate] = useState(new Date(2025, 0, 5));
+    const [checkedDates, setCheckedDates] = useState(new Map<string, boolean>([]));
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const parseDateString = (dateKey: string): Date => {
+        const [year, month, day] = dateKey.split('-').map(Number);
+        return new Date(year, month - 1, day);
+    };
+
+    const getDateString = (date: Date): string => date.toISOString().split("T")[0];
+
+    const isChecked = (date: Date): boolean => {
+        return checkedDates.get(getDateString(date)) || false;
+    }
+
+    const handleDayClick = (date: Date) => {
+        const key = getDateString(date);
+        const newCheckedDates = new Map(checkedDates);
+
+        if (isChecked(date)) {
+            newCheckedDates.delete(key);
+        } else {
+            newCheckedDates.set(key, true);
+        }
+        setCheckedDates(newCheckedDates);
+    }
+
+    return (
+        <>
+            <h3>Total days {checkedDates.size}</h3>
+            <input type="date" value={getDateString(minDate)}
+                   onChange={(e) => setMinDate(parseDateString(e.target.value))}/>
+            <Calendar onClickDay={handleDayClick}
+                      tileContent={({date}) => {
+                          return date >= minDate && date <= maxDate ? <div className='checkbox-wrapper-39'>
+                              <input type={'checkbox'} checked={isChecked(date)} onChange={() => handleDayClick(date)}/>
+                              <span className="checkbox"></span>
+                          </div> : undefined
+                      }}
+                      minDate={minDate}
+                      maxDate={maxDate}
+            />
+        </>
+    )
 }
 
 export default App
